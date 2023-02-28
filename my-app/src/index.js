125011ce23a4515045ca210f90651c7bd3b927bc
reidlum <reidlum@hawaii.edu>
2023-02-28 09:49:07 -1000

    navbar

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 const TopMenu = () => (
   <Navbar className="navbar-custom container" expand="lg">
-    <Container className="container-fluid">
+    <Container fluid={true}"container-fluid">
       <Nav className="me-auto container-fluid">
               <img src="https://images.squarespace-cdn.com/content/v1/5ee94b6da7910d1bee2546fd/1599597312683-IHIO1FA3OGWG31T9EHSA/murphyslogowhite.png?format=1500w" width="240px"/>
               <li className="nav-item px-3">Home</li>
